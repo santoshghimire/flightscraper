9f2cf0f41ff46bfd557a5044bc12a93b55f7d55d
Santosh Ghimire <santosh.ghimire33@gmail.com>
2017-01-27 15:56:30 +0545

    airasia change

--- routescraper/spiders/airasia.py
+++ routescraper/spiders/airasia.py
@@ -62,76 +62,83 @@ class AirAsiaSpider(scrapy.Spider):
         for p in prices:
             price = re.findall("\d+\.\d+", p)
             price_list.extend([float(i) for i in price])
-        min_price = min(price_list)
-        item['price'] = min_price
-        actual_min_price = prices[price_list.index(min_price)]
+        try:
+            min_price = min(price_list)
+        except:
+            self.logger.info('Flightinfo not available for {}'.format(depart))
+            min_price = None
+        if min_price is not None:
+            item['price'] = min_price
+            actual_min_price = prices[price_list.index(min_price)]
 
-        # find actual min flight element
-        detail_elem = response.xpath(
-            "//div[@class='avail-fare-price'][contains(text(),'" +
-            actual_min_price + "')]" +
-            "/ancestor::tr[@class='fare-dark-row']"
-        )
-        fare_elem = 'fare-dark-row'
-        if not detail_elem:
+            # find actual min flight element
             detail_elem = response.xpath(
                 "//div[@class='avail-fare-price'][contains(text(),'" +
                 actual_min_price + "')]" +
-                "/ancestor::tr[@class='fare-light-row']"
+                "/ancestor::tr[@class='fare-dark-row']"
             )
-            fare_elem = 'fare-light-row'
+            fare_elem = 'fare-dark-row'
+            if not detail_elem:
+                detail_elem = response.xpath(
+                    "//div[@class='avail-fare-price'][contains(text(),'" +
+                    actual_min_price + "')]" +
+                    "/ancestor::tr[@class='fare-light-row']"
+                )
+                fare_elem = 'fare-light-row'
 
-        # departure time
-        depart_time_elem = detail_elem.xpath(
-            ".//tr[@class='" + fare_elem + "']/td"
-            "[@class='avail-table-detail']"
-        )
-        if depart_time_elem:
-            depart_time = depart_time_elem[0].xpath(
-                ".//div[@class='avail-table-bold']/text()").extract_first()
-        else:
-            depart_time = ''
-        item['departure_date'] = depart + ' ' + depart_time
+            # departure time
+            depart_time_elem = detail_elem.xpath(
+                ".//tr[@class='" + fare_elem + "']/td"
+                "[@class='avail-table-detail']"
+            )
+            if depart_time_elem:
+                depart_time = depart_time_elem[0].xpath(
+                    ".//div[@class='avail-table-bold']/text()").extract_first()
+            else:
+                depart_time = ''
+            item['departure_date'] = depart + ' ' + depart_time
 
-        # arrival time
-        arrive_time_elem = detail_elem.xpath(
-            ".//tr[@class='" + fare_elem + "']/td"
-            "[@class='avail-table-detail']"
-        )
-        if arrive_time_elem:
-            arrive_time = arrive_time_elem[1].xpath(
-                ".//div[@class='avail-table-bold']/text()"
+            # arrival time
+            arrive_time_elem = detail_elem.xpath(
+                ".//tr[@class='" + fare_elem + "']/td"
+                "[@class='avail-table-detail']"
+            )
+            if arrive_time_elem:
+                arrive_time = arrive_time_elem[1].xpath(
+                    ".//div[@class='avail-table-bold']/text()"
+                ).extract_first()
+            else:
+                arrive_time = ''
+            # arrival day
+            arrive_day = detail_elem.xpath(
+                ".//tr[@class='" + fare_elem +
+                "']/td[@class='avail-table-detail']"
+            )[1].xpath(
+                ".//div[@class='avail-table-next-day']/text()"
             ).extract_first()
-        else:
-            arrive_time = ''
-        # arrival day
-        arrive_day = detail_elem.xpath(
-            ".//tr[@class='" + fare_elem + "']/td[@class='avail-table-detail']"
-        )[1].xpath(
-            ".//div[@class='avail-table-next-day']/text()"
-        ).extract_first()
-        departure_date_obj = datetime.strptime(depart, "%Y-%m-%d")
-        arrival_date_obj = departure_date_obj
-        if arrive_day == '+1':
-            arrival_date_obj = departure_date_obj + timedelta(1)
-        item['arrival_date'] = arrival_date_obj.strftime("%Y-%m-%d") + ' ' +\
-            arrive_time
+            departure_date_obj = datetime.strptime(depart, "%Y-%m-%d")
+            arrival_date_obj = departure_date_obj
+            if arrive_day == '+1':
+                arrival_date_obj = departure_date_obj + timedelta(1)
+            item['arrival_date'] = arrival_date_obj.strftime("%Y-%m-%d") +\
+                ' ' + arrive_time
 
-        item['price_date'] = datetime.now().strftime('%Y-%m-%d')
-        item['days_to_flight'] = (departure_date_obj - datetime.today()).days
+            item['price_date'] = datetime.now().strftime('%Y-%m-%d')
+            item['days_to_flight'] = (
+                departure_date_obj - datetime.today()).days
 
-        flight_num = detail_elem.xpath(
-            ".//div[@class='carrier-hover-bold']/text()").extract_first()
-        item['flight_number'] = flight_num
-        career_symbol = flight_num[:2]
-        carriers = response.xpath(
-            "//span[@class='carrier-legend-desc']/text()").extract()
-        carriers = [i.strip() for i in carriers]
-        airline_name = [
-            i for i in carriers if i.startswith(career_symbol + ' -')]
-        if airline_name:
-            item['airline_name'] = airline_name[0]
-        else:
-            item['airline_name'] = ''
-        item['fare_class'] = ''
-        yield item
+            flight_num = detail_elem.xpath(
+                ".//div[@class='carrier-hover-bold']/text()").extract_first()
+            item['flight_number'] = flight_num
+            career_symbol = flight_num[:2]
+            carriers = response.xpath(
+                "//span[@class='carrier-legend-desc']/text()").extract()
+            carriers = [i.strip() for i in carriers]
+            airline_name = [
+                i for i in carriers if i.startswith(career_symbol + ' -')]
+            if airline_name:
+                item['airline_name'] = airline_name[0]
+            else:
+                item['airline_name'] = ''
+            item['fare_class'] = ''
+            yield item
